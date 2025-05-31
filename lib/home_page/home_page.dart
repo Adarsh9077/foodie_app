b4032c4d7f12857153cc4fbedcc128a84eaaf9f1
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-05-31 11:48:33 +0530

    working on Home Screen

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -10,6 +10,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: SizedBox(
+          height: 56,
+          width: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IconBottomBar extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final bool isSelected;
+
+  final Function() onTap;
+
+  const IconBottomBar({
+    required this.text,
+    required this.icon,
+    this.isSelected = false,
+    required this.onTap,
+    Key? key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Placeholder();
   }
 }
