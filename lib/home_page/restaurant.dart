1103a66f0ea0e1c7fb7d4461b98f47c7f6079701
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-06-05 11:23:42 +0530

    work on Restaurant Page

--- lib/home_page/restaurant.dart
+++ lib/home_page/restaurant.dart
@@ -8,3 +8,27 @@ class Restaurant extends StatelessWidget {
     return Placeholder();
   }
 }
+
+Widget restaurantAvailable(BuildContext context) {
+  Size size = MediaQuery.of(context).size;
+  return Container(
+    width: size.width,
+    height: size.height,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromARGB(255, 83, 69, 164),
+          Color.fromARGB(125, 66, 53, 165),
+          Color.fromARGB(95, 75, 53, 165),
+          Color.fromARGB(95, 121, 112, 159),
+          Color.fromARGB(45, 70, 53, 165),
+          Color(0xff6F35A5).withAlpha(30),
+          Color(0xff6F35A5).withAlpha(25),
+          Color(0xff6F35A5).withAlpha(10),
+        ],
+      ),
+    ),
+  );
+} // 10:40 part 3
