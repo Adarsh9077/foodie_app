4611d5f691398d8edc9f87842caace8a91a26fe7
Adarsh9077 <bhardwajadarsh873@gmail.com>
2025-06-16 23:10:27 +0530

    working on sign Up credential

--- lib/signup/components/signup_form.dart
+++ lib/signup/components/signup_form.dart
@@ -1,73 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/components/already_have_an_account_acheck.dart';
-import 'package:foodie/login/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:foodie/ui_helper/utils.dart';
 import 'package:foodie/widgets/constants.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
   const SignUpForm({super.key});
 
   @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  void _signUpWithEmailAndPassword() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+          email: _emailController.text, password: _passwordController.text);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: defaultPadding),
-        TextField(
-          keyboardType: TextInputType.emailAddress,
-          textInputAction: TextInputAction.next,
-          cursorColor: kPrimaryColor,
-          onSubmitted: (email) {},
-          decoration: InputDecoration(
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(defaultPadding),
-              child: Icon(Icons.person_rounded),
-            ),
-            hintText: "Email ID",
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(vertical: defaultPadding),
-          child: TextField(
-            obscureText: true,
-            obscuringCharacter: "*",
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SizedBox(height: defaultPadding),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
             cursorColor: kPrimaryColor,
-            textInputAction: TextInputAction.done,
+            onSaved: (email) {},
             decoration: InputDecoration(
-              hintText: "Your Password",
               prefixIcon: Padding(
-                padding: EdgeInsets.all(defaultPadding),
-                child: Icon(Icons.lock),
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person_rounded),
               ),
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.remove_red_eye_outlined),
+              hintText: "Email ID",
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              obscureText: true,
+              obscuringCharacter: "*",
+              cursorColor: kPrimaryColor,
+              textInputAction: TextInputAction.done,
+              decoration: InputDecoration(
+                hintText: "Your Password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.remove_red_eye_outlined),
+                ),
               ),
             ),
           ),
-        ),
-        SizedBox(height: defaultPadding),
-        Hero(
-          tag: "signup_btn",
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "Sign Up".toUpperCase(),
-              style: mTextStyle20(
-                txtColor: Colors.white,
-                fontWeight: FontWeight.w800,
+          SizedBox(height: defaultPadding),
+          Hero(
+            tag: "signup_btn",
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Sign Up".toUpperCase(),
+                style: mTextStyle20(
+                  txtColor: Colors.white,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
             ),
           ),
-        ),
-        SizedBox(height: defaultPadding),
-        AlreadyHaveAnAccountCheck(
-          login: false,
-          press: () {
-            Navigator.pop(context);
-          },
-        ),
-        // SizedBox(height: defaultPadding),
-      ],
+          SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            login: false,
+            press: () {
+              Navigator.pop(context);
+            },
+          ),
+          // SizedBox(height: defaultPadding),
+        ],
+      ),
     );
   }
-}
+} // 52:20 part 6
